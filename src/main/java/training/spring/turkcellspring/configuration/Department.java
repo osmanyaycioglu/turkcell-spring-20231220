91ce6f9f0b315a0cffe95f917785da9601e4781d
osman yaycioglu <osman.yaycioglu@gmail.com>
2023-12-20 14:07:21 +0300

    day 1 3

---
+++ src/main/java/training/spring/turkcellspring/configuration/Department.java
@@ -0,0 +1,11 @@
+package training.spring.turkcellspring.configuration;
+
+import lombok.Data;
+
+@Data
+public class Department {
+    private String name;
+    private String location;
+    private Long distance;
+
+}
--- src/main/java/training/spring/turkcellspring/configuration/MyAppProperties.java
+++ src/main/java/training/spring/turkcellspring/configuration/MyAppProperties.java
@@ -4,13 +4,19 @@ import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+import java.util.Map;
+
 @Component
 @ConfigurationProperties(prefix = "my.app")
 @Data
 public class MyAppProperties {
-    private String appVersion;
-    private String appTest;
-    private String defaultLanguage;
-    private long   responseTimeout;
-
+    private String                  appVersion;
+    private String                  appTest;
+    private String                  defaultLanguage;
+    private long                    responseTimeout;
+    private Department              department;
+    private List<String>            defaultNames;
+    private List<Department>        departments;
+    private Map<String, Department> departmentMap;
 }
